a946881c34ee59d81c99a8b56eef7b66295616cb
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-07-08 14:08:32 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,14 +1,49 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NonNull;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class Film {
-    private long id = 0;
+    private long  id = 0;
     private @NonNull String name;
     private @NonNull String description;
     private @NonNull LocalDate releaseDate;
     private @NonNull int duration;
+    private long countLike = 0;
+    private Set<Long> usersId = new HashSet<>();
+
+    // Для десереализация при GET-запросе
+    public Film() {
+        super();
+    }
+
+    public Film(@NonNull String name, @NonNull String description, @NonNull LocalDate releaseDate,
+                @NonNull int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
+
+    public void addLike() {
+        this.countLike++;
+    }
+
+    public void removeLike() {
+        this.countLike--;
+    }
+
+    public void addUserId(long userId) {
+        usersId.add(userId);
+    }
+
+    public void removeUserId(long userId) {
+        usersId.remove(userId);
+    }
 }
