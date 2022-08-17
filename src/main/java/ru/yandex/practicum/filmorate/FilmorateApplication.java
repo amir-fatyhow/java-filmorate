f6770e73425144c194ecc477ec3169281b21d682
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-08-17 17:49:11 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,11 +3,15 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FilmorateApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(FilmorateApplication.class, args);
-
+//TODO - Описание проекта и подсказки.
+/**
+ * queryForObject - используется там, где возвращается всегда одно значение!
+ * Самый быстрый и оптимальный вариант работы с запросами - через JOIN, лучше отказаться от подзапросов!
+ * Все запросы к БД выполняются очень медленно, по-этому лучше использовать индексы для большой БД.
+ */
+    @SpringBootApplication
+    public class FilmorateApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(FilmorateApplication.class, args);
+        }
     }
-}
-
